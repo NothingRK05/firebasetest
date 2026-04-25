e1c9d366197131190b6db5550ead6f08f3e0eb4b
RK <ricks3558@gmail.com>
2026-04-25 15:33:36 -0500

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -28,7 +28,7 @@ const app     = express();
 const cors    = require("cors");
 
 app.use(express.json());
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '../static'));
 app.use(cors({ origin: '*' }));
 
 const port = process.env.PORT || 8080;
