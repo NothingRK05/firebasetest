bf0091d3aa8ebba074ea6876856e108e13782534
RK <ricks3558@gmail.com>
2026-04-26 07:53:43 -0500

    added login redirect

--- react-frontend/src/components/Login.jsx
+++ react-frontend/src/components/Login.jsx
@@ -2,30 +2,41 @@
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.js";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const { currentUser } = useAuth();
+
+    if (currentUser) {
+        navigate("/dashboard");
+    }
 
     const handleLogin = async () => {
         try {
             await signInWithEmailAndPassword(auth, email, password);
-            alert("Logged in!");
+            navigate("/dashboard");
         } catch (err) {
             alert(err.message);
         }
     };
-
+    // future add confirm password(line 39), add username
     return (
         <div>
             <h2>Login</h2>
-            <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input 
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)} 
+            />
             <input
                 placeholder="Password"
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
             />
+
             <button onClick={handleLogin}>Login</button>
             <p>
                 Don’t have an account? <Link to="/signup">Sign up</Link>
